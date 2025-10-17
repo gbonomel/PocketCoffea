56c2678e9d930628d1732773e7853b2f9b9bdd39
Giorgia Bonomelli <gbonomel122@gmail.com>
2025-10-17 15:42:09 +0200

    added function to use TTHmva ID score to select electrons and lepton selection for medium but not tight leptons (used in the MR for lepton fakes)

--- pocket_coffea/lib/leptons.py
+++ pocket_coffea/lib/leptons.py
@@ -203,6 +203,64 @@ def lepton_selection(events, lepton_flavour, params):
 
     return leptons[good_leptons]
 
+def lepton_selection_mvaTTH(events, lepton_flavour, params):
+
+    leptons = events[lepton_flavour]
+    cuts = params.object_preselection[lepton_flavour]
+    # Requirements on pT and eta
+    passes_eta = abs(leptons.eta) < cuts["eta"]
+    passes_pt = leptons.pt > cuts["pt"]
+
+    if lepton_flavour == "Electron":
+        # Requirements on SuperCluster eta, isolation and id
+        etaSC = abs(leptons.deltaEtaSC + leptons.eta)
+        passes_SC = np.invert((etaSC >= 1.4442) & (etaSC <= 1.5660))
+        passes_iso = True
+        if "iso" in cuts.keys():
+            passes_iso = leptons.pfRelIso03_all < cuts["iso"]
+        passes_id = (leptons[cuts['id']] > cuts['wp']['tight'])
+
+        good_leptons = passes_eta & passes_pt & passes_SC & passes_iso & passes_id
+
+    elif lepton_flavour == "Muon":
+        # Requirements on isolation and id
+        passes_iso = leptons.pfRelIso04_all < cuts["iso"]
+        passes_id  = leptons[cuts['id']] == True
+
+        good_leptons = passes_eta & passes_pt & passes_iso & passes_id
+
+    return leptons[good_leptons]
+
+def lepton_selection_loose(events, lepton_flavour, params):
+
+    leptons = events[lepton_flavour]
+    cuts = params.object_preselection[lepton_flavour+"_loose"]
+    # Requirements on pT and eta
+    passes_eta = abs(leptons.eta) < cuts["eta"]
+    passes_pt = leptons.pt > cuts["pt"]
+
+    if lepton_flavour == "Electron":
+        # Requirements on SuperCluster eta, isolation and id
+        etaSC = abs(leptons.deltaEtaSC + leptons.eta)
+        passes_SC = np.invert((etaSC >= 1.4442) & (etaSC <= 1.5660))
+        passes_iso = True
+        if "iso" in cuts.keys():
+            passes_iso = leptons.pfRelIso03_all < cuts["iso"]
+        passes_id_medium = (leptons[cuts['id']] > cuts['wp']['medium'])
+        fails_id_tight = (leptons[cuts['id']] < cuts['wp']['tight'])
+
+        good_leptons = passes_eta & passes_pt & passes_SC & passes_iso & passes_id_medium & fails_id_tight
+
+    elif lepton_flavour == "Muon":
+        # Requirements on isolation and id
+        passes_iso = leptons.pfRelIso04_all < cuts["iso"]
+        passes_id_loose = leptons[cuts['id_loose']] == True
+        fails_id_tight  = leptons[cuts['id_tight']] == False
+
+        good_leptons = passes_eta & passes_pt & passes_iso & passes_id_loose & fails_id_tight
+
+    return leptons[good_leptons]
+
 def soft_lepton_selection(events, lepton_flavour, params):
 
     leptons = events[lepton_flavour]
